6d17c8500469dbe00697224226f6eb8efa145dbe
Shamsiddin-it <samsiddinarbobov@gmail.com>
2024-08-28 13:37:23 +0500

    z1

---
+++ hm1.py
@@ -0,0 +1,14 @@
+# import datetime
+# def halowen(a):
+#     year, month, day=map(int, a)
+#     new=datetime.date(year, month, day)
+#     if new.day==31 and new.month==10:
+#         print("Bonfire toffee")
+#     else:
+#         print("toffee")
+# a=input()
+# b=a.split('/')
+# halowen(b)
+
+# ---------------------------------------------
+
